83c554ce5e3b86261a37ec7084c1e87c61acef9e
agusharyokp <agusharyokp@gmail.com>
2025-05-17 12:47:08 +0700

    Add signup and login feature, and implement JWT

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require('dotenv').config();
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -45,6 +46,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const { body } = require("express-validator");
+const User = require("../models/user");
+
+const authController = require("../controllers/auth");
+
+router.put("/signup", [
+    body("name")
+        .isLength({ min: 5 })
+        .withMessage("Name must be at least 5 characters long"),
+    body("email")
+        .isEmail()
+        .withMessage("Please enter a valid email.")
+        .custom((value, { req }) => {
+            return User.findOne({ email: value }).then((userDoc) => {
+                if (userDoc) {
+                    return Promise.reject("Email already exists.");
+                }
+            });
+        })
+        .normalizeEmail(),
+    body("password")
+        .isLength({ min: 5 })
+        .withMessage("Password must be at least 5 characters long"),
+], authController.signup);
+
+router.post("/login", authController.login);
+
+module.exports = router;
